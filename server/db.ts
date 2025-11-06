a3fb52bf907ba2a4a52e207cb09b98a9009d3685
jt7991 <jt7991@gmail.com>
2025-11-06 07:13:18 -0500

    use pg

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,6 @@
-import { neonConfig, Pool } from "@neondatabase/serverless";
 import * as schema from "@shared/schema";
-import { drizzle } from "drizzle-orm/neon-serverless";
-import ws from "ws";
-
-neonConfig.webSocketConstructor = ws;
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 if (!process.env.DATABASE_URL) {
 	throw new Error(
