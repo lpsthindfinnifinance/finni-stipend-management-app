f047fd95a3af0b607ffb0584e14621fc900914d3
lpsthindfinnifinance <lovepreet.thind@finnihealth.com>
2025-11-08 15:05:36 -0500

    Update schema.ts
    added 'sub' in db schema

--- shared/schema.ts
+++ shared/schema.ts
@@ -32,6 +32,7 @@ export const sessions = pgTable(
 // User storage table (MANDATORY for Replit Auth)
 export const users = pgTable("users", {
   id: varchar("id").primaryKey().default(sql`gen_random_uuid()`),
+  sub: varchar("sub").unique(),
   email: varchar("email").unique(),
   firstName: varchar("first_name"),
   lastName: varchar("last_name"),
